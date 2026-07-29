a83b02f3c4fd3e97b0bc0c67a994e5af718857df
Rosie Cook <rose.cook969@gmail.com>
2026-07-29 16:47:06 +0100

    fix tags

--- src/main/java/com/rosemods/windswept_delights/core/data/server/tags/WDItemTagsProvider.java
+++ src/main/java/com/rosemods/windswept_delights/core/data/server/tags/WDItemTagsProvider.java
@@ -22,8 +22,8 @@ public class WDItemTagsProvider extends ItemTagsProvider {
     @Override
     protected void addTags(Provider provider) {
         this.tag(ModTags.Items.CABINETS_WOODEN).add(WDBlocks.HOLLY_CABINET.get().asItem(), WDBlocks.CHESTNUT_CABINET.get().asItem(), WDBlocks.PINE_CABINET.get().asItem());
-        this.tag(TagUtil.itemTag("c", "raw_meat")).add(WDItems.GOAT_SHANKS.get());
-        this.tag(TagUtil.itemTag("c", "raw_goat")).add(WDItems.GOAT_SHANKS.get());
+        this.tag(TagUtil.itemTag("c", "foods/raw_meat")).add(WDItems.GOAT_SHANKS.get());
+        this.tag(TagUtil.itemTag("c", "foods/raw_goat")).add(WDItems.GOAT_SHANKS.get());
         this.tag(TagUtil.itemTag("c", "cooked_meat")).add(WDItems.COOKED_GOAT_SHANKS.get());
         this.tag(TagUtil.itemTag("c", "cooked_goat")).add(WDItems.COOKED_GOAT_SHANKS.get());
     }
